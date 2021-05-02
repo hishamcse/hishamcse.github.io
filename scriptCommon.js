33a425026d660ab86c526d89235ec0aa3606b2cc
hishamcse <1805004@ugrad.cse.buet.ac.bd>
2021-05-02 22:50:23 +0600

    Added Effects

--- scriptCommon.js
+++ scriptCommon.js
@@ -16,3 +16,27 @@ const fadeNav = function (e) {
 
 navBar.addEventListener('mouseover', fadeNav.bind(0.5));
 navBar.addEventListener('mouseout', fadeNav.bind(1));
+
+/*--------------------- Reveal Elements on scroll -------------------*/
+
+const sections = document.querySelectorAll('.row');
+
+const revealObserverCallBack = (entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+
+        entry.target.classList.remove('row--hidden');
+        observer.unobserve(entry.target);                  // as we don't need them after revealing once
+    });
+};
+
+const revealObserverOptions = {
+    root: null,
+    threshold: 0.1
+};
+
+const revealObserver = new IntersectionObserver(revealObserverCallBack, revealObserverOptions);
+sections.forEach(section => {
+    section.classList.add('row--hidden');
+    revealObserver.observe(section);
+});
